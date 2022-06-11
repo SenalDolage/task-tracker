f9f968909437d125831da93408313e91cc579f77
SenalDolage <senal.dolage@gmail.com>
2022-06-12 01:38:43 +0530

    controller functions and Task model added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,68 @@
-const getAllTasks = (req, res) => {
-  res.send("all items");
+const Task = require("../models/task");
+
+// Getting All Tasks
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(201).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.send("getTask");
+// Get Task By Id
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findById({ _id: taskId });
+    if (!task) {
+      res.send(404).json({ message: "Task not found" });
+    }
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
-const createTask = (req, res) => {
-  res.send("all items1");
+// Create a new Task
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("createTask");
+// Update a Task
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      res.send(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ success: true, task });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("createTask");
+// Delete a Task
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      res.send(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ sucess: true });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
 module.exports = {
