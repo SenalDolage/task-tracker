fdac5be1c591bacaacdd46a70bb8babafc2494d7
SenalDolage <senal.dolage@gmail.com>
2022-06-12 17:44:08 +0530

    swagger documentation added

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,7 +8,133 @@ const {
   deleteTask,
 } = require("../controllers/tasks");
 
+// Define Schema
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Task:
+ *        type: object
+ *        required:
+ *          - name
+ *          - completed
+ *        properties:
+ *         name:
+ *          type: string
+ *          description: The name of the Task,
+ *         completed:
+ *          type: boolean
+ *          description: The status of the Task.
+ *        example:
+ *          name: Walk the dog.
+ *          completed: true
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: API to manage Tasks
+ */
+
+// Get All, Post Documentation
+/**
+ * @swagger
+ *  /:
+ *   get:
+ *    summary: Gets all the Tasks
+ *    tags: [Tasks]
+ *    responses:
+ *      200:
+ *        description: Success
+ *   post:
+ *     summary: Create a new Task
+ *     tags: [Tasks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       200:
+ *         description: Task created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       500:
+ *         description: Server error
+ */
+
 router.route("/").get(getAllTasks).post(createTask);
+
+// Get by Id, Patch, Delete Documentation
+/**
+ * @swagger
+ *  /{id}:
+ *   get:
+ *    summary: Get Task by Id
+ *    tags: [Tasks]
+ *    parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *    responses:
+ *      200:
+ *        description: Success
+ *        contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *      404:
+ *        description: The task was not found
+ *   patch:
+ *    summary: Update the Task by id
+ *    tags: [Tasks]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The task id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Task'
+ *    responses:
+ *      200:
+ *        description: The task was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Task'
+ *      404:
+ *        description: The task was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the Task by id
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *     responses:
+ *       200:
+ *         description: The task was deleted
+ *       404:
+ *         description: The task was not found
+ */
 
 router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
--- server.js
+++ server.js
@@ -1,16 +1,54 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const app = express();
+
 const tasks = require("./routes/tasks");
 const connectDb = require("./database/connect");
-const port = 3000;
+const PORT = 3000;
 
+// Connecting to DB
 connectDb();
+
+// API documentation options
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Task Manager ExpressJS API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Senal Dolage",
+        url: "https://senaldolage.com",
+        email: "senal.dolage@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1/tasks",
+      },
+    ],
+  },
+  apis: ["./routes/tasks.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { explorer: true })
+);
+
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/tasks", tasks);
 
-app.get("/hello", (req, res) => {
-  res.send("Task Manager");
-});
-
-app.listen(port, console.log(`server is listening in ${port}`));
+app.listen(PORT, console.log(`server is listening in ${PORT}`));
